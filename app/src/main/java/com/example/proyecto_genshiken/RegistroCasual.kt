13179e4d5e9064cc54de68df3ac14d7876a32148
HamzaElKha04 <hamzaelkhatta04@gmail.com>
2026-05-04 17:50:14 +0200

    Limpieza final de estructura del repositorio

--- app/src/main/java/com/example/proyecto_genshiken/RegistroCasual.kt
+++
@@ -1,8 +0,0 @@
-package com.example.proyecto_genshiken
-
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-
-fun RegistroCasual(navController: NavHostController){
-
-}
--- app/src/main/java/com/example/proyecto_genshiken/UserSession.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyecto_genshiken
-
-//Aqui es como guardamos la sesion de un usuario, en inicio sesion cuando el usuario se loguee en la aplicacion esta guardara su nombre y su id de usuario segun la base de datos. esto se seguira guardando hasta que el usuario cierre la aplicacion
-object UserSession {
-    var userId: Int = 0
-    var userName: String = ""
-}
