02abd59b187d58cb3f532e76523ef0bd2fe5254a
Hamza El Khattabi <hamzaelkhatta04@gmail.com>
2026-05-08 13:14:59 +0200

    pruebass

--- Proyecto-Genshiken-Hamza-El-Khattabi/web/admin/ver_respuestas.php
+++ Proyecto-Genshiken-Hamza-El-Khattabi/web/admin/ver_respuestas.php
@@ -3,7 +3,7 @@
 --------------------------------------------------
 Panel de administración - Ver respuestas
 --------------------------------------------------
-
+PRUEBASS
 Esta página permite consultar las respuestas
 asociadas a una pregunta concreta del juego.
 
